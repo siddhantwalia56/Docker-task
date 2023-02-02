07ddea8bf025e05fa82c2c1d8f0438683e4aeb93
Siddhant Walia <siddhantwalia56@gmail.com>
2023-02-02 23:56:04 +0530

    worked on shopping component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-ingredients=[];
+ingredients: Ingredient[]=[
+  new Ingredient('Apple',5),
+  new Ingredient('Tomato',4)
+];
 }
