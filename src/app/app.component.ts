945d34d452b3e2fe8ab82cd13ee7bafb18531358
Siddhant Walia <siddhantwalia56@gmail.com>
2023-02-07 16:37:26 +0530

    Navigation with EventBinding and Passing recipe with property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dummy-app';
+loadedfeature='recipe'
+  onNavigate(feature:string){
+    this.loadedfeature=feature
+  }
 }
